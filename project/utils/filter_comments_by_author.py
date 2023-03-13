593e2eac8fae52916f07a1eabd30beee4a294596
ek-krupnik <ek.krupnik@yandex.ru>
2023-03-13 18:50:21 +0300

    for review

--- project/utils/filter_comments_by_author.py
+++ project/utils/filter_comments_by_author.py
@@ -1,2 +1,4 @@
-def filter_comments_by_author(comments, author):
+from models.user import User
+
+def filter_comments_by_author(comments: list, author: User) -> list:
     return [comment for comment in comments if comment.author_id == author.id]
--- project/utils/get_ordered_products_by_price.py
+++ project/utils/get_ordered_products_by_price.py
@@ -1,2 +1,2 @@
-def get_ordered_products_by_price(products):
+def get_ordered_products_by_price(products: list) -> list:
     return sorted(products, key=lambda product: product.get_price(), reverse=True)
